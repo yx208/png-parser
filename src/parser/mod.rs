7a104962c7f7cc7a3d71129bf5120ac03352d41c
Jialiang <yx208.lin@gmail.com>
2023-11-02 18:19:53 +0800

    update

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,7 +3,6 @@ extern crate crc32fast;
 use std::convert::TryInto;
 use std::fs::read;
 use std::io::Read;
-use std::ops::Sub;
 
 use flate2::read::ZlibDecoder;
 
@@ -27,6 +26,7 @@ struct PngParam {
 
 impl PngParam {
     fn new(raw_body: &[u8]) -> Self {
+
         let width = u32::from_be_bytes((&raw_body[0..4]).try_into().unwrap());
         let height = u32::from_be_bytes((&raw_body[4..8]).try_into().unwrap());
         let depth = raw_body[8];
@@ -69,6 +69,7 @@ impl PhysParam {
 }
 
 /// 暂时只考虑真彩色图片
+#[derive(Clone)]
 struct Pixel {
     r: u8,
     g: u8,
@@ -76,23 +77,47 @@ struct Pixel {
     a: u8
 }
 
-impl Sub for Pixel {
-    type Output = Self;
+impl Pixel {
 
-    fn sub(self, rhs: Self) -> Pixel {
+    fn sub(&self, rhs: &Pixel) -> Pixel {
         Pixel {
-            r: self.r - rhs.r,
-            g: self.g - rhs.g,
-            b: self.b - rhs.b,
-            a: self.a - rhs.a
+            r: self.sub_filter(self.r, rhs.r),
+            g: self.sub_filter(self.g, rhs.g),
+            b: self.sub_filter(self.b, rhs.b),
+            a: self.sub_filter(self.a, rhs.a)
         }
+    }
+
+    fn add(&self, rhs: &Pixel) -> Pixel {
+        Pixel {
+            r: self.sub_filter(self.r, rhs.r),
+            g: self.sub_filter(self.g, rhs.g),
+            b: self.sub_filter(self.b, rhs.b),
+            a: self.sub_filter(self.a, rhs.a)
+        }
+    }
+
+    fn to_u32(&self) -> u32 {
+        ((self.r as u32) << 24) | ((self.g as u32) << 16) | ((self.b as u32) << 8) | (self.a as u32)
+    }
+
+    #[inline]
+    fn sub_filter(&self, a: u8, b: u8) -> u8 {
+        a.wrapping_sub(b)
+        // ((a as i32) - (b as i32)).abs() as u8
+    }
+
+    #[inline]
+    fn add_filter(&self, a: u8, b: u8) -> u8 {
+        a.wrapping_add(b)
+        // ((a as i32) - (b as i32)).abs() as u8
     }
 }
 
 struct Scanline {
     filter: u8,
     color_channel: usize,
-    data: Vec<u8>
+    data: Vec<Pixel>
 }
 
 impl Scanline {
@@ -112,62 +137,60 @@ impl Scanline {
             panic!("扫描线长度异常");
         }
 
-        // let mut data: Vec<Pixel> = Vec::new();
-        // let pixels = (bytes.len() - 1) / color_channel;
-        // for pixel in 0..pixels {
-        //     let index = pixel * color_channel + 1;
-        //     data.push(Pixel {
-        //         r: bytes[index],
-        //         g: bytes[index + 1],
-        //         b: bytes[index + 2],
-        //         a: bytes[index + 3]
-        //     });
-        // }
+        let mut data: Vec<Pixel> = Vec::new();
+        let pixels = (bytes.len() - 1) / color_channel;
+        for pixel in 0..pixels {
+            let index = pixel * color_channel + 1;
+            data.push(Pixel {
+                r: bytes[index],
+                g: bytes[index + 1],
+                b: bytes[index + 2],
+                a: bytes[index + 3]
+            });
+        }
 
         Scanline {
+            data,
             color_channel,
-            data: bytes[1..].to_vec(),
             filter: bytes[0],
         }
     }
 
-    fn decode_sub(&self) -> Vec<u32> {
-
-        let mut result: Vec<u32> = Vec::new();
-
-        #[inline]
-        fn sub_filter(a: u8, b: u8) -> u8 {
-            let c = a as i32 - b as i32;
-            c.abs() as u8
-        }
-
-        // [255, 0, 0, 255]
-        // [1, 255, 0, 0]
-
-        let mut previous: [u8; 4] = [0, 0, 0, 0];
-        let pixels = self.data.len() / self.color_channel;
-        for pixel in 0..pixels {
-            let index = pixel * self.color_channel;
-
-            previous[0] = sub_filter(self.data[index], previous[0]);
-            previous[1] = sub_filter(self.data[index + 1], previous[1]);
-            previous[2] = sub_filter(self.data[index + 2], previous[2]);
-            previous[3] = sub_filter(self.data[index + 3], previous[3]);
-
-            let decode = rgba_to_u32(&previous);
-
-            // if self.filter == 1 {
-            //     println!("{:?}", previous);
-            // }
-
-            result.push(decode);
-        }
-
-        // if self.filter == 1 {
-        //     println!("{:?}", result);
-        // }
+    fn decode_sub(&self) -> Vec<Pixel> {
+        let mut previous = Pixel { r: 0, g: 0, b: 0, a: 0 };
+        let result: Vec<Pixel> = self.data.iter()
+            .map(|pixel| {
+                previous = pixel.sub(&previous);
+                previous.clone()
+            })
+            .collect();
 
         result
+    }
+
+    fn decode_up(&self, previous: &Vec<Pixel>) -> Vec<Pixel> {
+        self.data.iter().enumerate().map(|(index, pixel)| {
+            pixel.sub(previous.get(index).unwrap())
+        }).collect()
+    }
+
+    fn decode_paeth(&self, previous: &Vec<Pixel>) -> Vec<Pixel> {
+        let mut temp = Pixel { r: 0, g: 0, b: 0, a: 0 };
+        let result: Vec<Pixel> = self.data.iter()
+            .map(|pixel| {
+                temp = pixel.sub(&temp);
+                temp.clone()
+            })
+            .collect();
+
+        result
+    }
+
+    fn paeth_predictor(&self, a: &Pixel, b: &Pixel, c: &Pixel) {
+        let p = a.add(b).sub(c);
+        let pa = p.sub(a);
+        let pb = p.sub(b);
+        let pc = p.sub(c);
     }
 
 }
@@ -264,8 +287,6 @@ impl PngParser {
 
     fn parse_idat_block(&self, raw_body: &[u8]) {
 
-        println!("{:?}", &self.params);
-
         let mut decoder = ZlibDecoder::new(raw_body);
         let mut decode_data = Vec::new();
         decoder.read_to_end(&mut decode_data).expect("解压 IDAT 块数据失败");
@@ -274,24 +295,88 @@ impl PngParser {
             panic!("Not params");
         };
 
-        let scanline_vec: Vec<u32> = decode_data
+        let mut scanline_list: Vec<Scanline> = decode_data
             .chunks((1 + 4 * params.width) as usize)
-            .map(|chunk| Scanline::new(chunk, params.color_type).decode_sub())
-            .flatten()
+            .map(|chunk| Scanline::new(chunk, params.color_type))
             .collect();
 
-        let demo = scanline_vec[0..398].to_owned();
-        let mut rr = Vec::new();
-        for _ in 0..398 {
-            rr.append(&mut demo.clone());
+        let mut result: Vec<Vec<Pixel>> = Vec::new();
+        for (index, scanline) in scanline_list.iter_mut().enumerate() {
+            match scanline.filter {
+                1 => {
+                    result.push(scanline.decode_sub());
+                },
+                2 => {
+                    let line = scanline.decode_up(result.get_mut(index - 1).unwrap());
+                    result.push(line);
+                }
+                4 => {
+                    let line = scanline.decode_paeth(result.get_mut(index - 1).unwrap());
+                    result.push(line);
+                }
+                _ => {}
+            };
         }
 
-        render_image(398, 398, &rr);
+        // let scanline_vec: Vec<u32> = decode_data
+        //     .chunks((1 + 4 * params.width) as usize)
+        //     .map(|chunk| Scanline::new(chunk, params.color_type).decode_sub())
+        //     .flatten()
+        //     .collect();
+        //
+        // let demo = scanline_vec[0..398].to_owned();
+        // let mut rr = Vec::new();
+        // for _ in 0..398 {
+        //     rr.append(&mut demo.clone());
+        // }
+        //
+        // render_image(398, 398, &rr);
 
         // for item in scanline_vec {
         //     // println!("{}", item.filter);
         // }
 
+    }
+
+    fn map_color_type_to_channel(color_type: u8) -> usize {
+         match color_type {
+            0 => 1,
+            2 => 3,
+            3 => 1,
+            4 => 2,
+            6 => 4,
+            _ => panic!("错误的颜色类型")
+        }
+    }
+
+    fn decode_sub(&self, pixel_data: &[u8]) -> Vec<u32> {
+
+        let Some(params) = &(self.params) else { panic!(""); };
+        let channel = PngParser::map_color_type_to_channel(params.color_type);
+        let mut result: Vec<u32> = Vec::new();
+
+        #[inline]
+        fn sub_filter(a: u8, b: u8) -> u8 {
+            let c = a as i32 - b as i32;
+            c.abs() as u8
+        }
+
+        let mut previous: [u8; 4] = [0, 0, 0, 0];
+        let pixels = pixel_data.len() / channel;
+        for pixel in 0..pixels {
+            let index = pixel * channel;
+
+            previous[0] = sub_filter(pixel_data[index], previous[0]);
+            previous[1] = sub_filter(pixel_data[index + 1], previous[1]);
+            previous[2] = sub_filter(pixel_data[index + 2], previous[2]);
+            previous[3] = sub_filter(pixel_data[index + 3], previous[3]);
+
+            let decode = rgba_to_u32(&previous);
+
+            result.push(decode);
+        }
+
+        result
     }
 
 }
